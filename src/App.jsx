8eebe63f32ed0b13f0efb1c657c0769bc130cbcf
Edson Ribeiro <edson.junior@kabum.com.br>
2022-05-11 22:22:07 -0300

    refatorando layout do component

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,51 @@ import Container from '@mui/material/Container'
 
 function App() {
 
+  let cupoms = [
+    {
+      primeNinja: true,
+      desconto: '5%',
+      categoria: '',
+      cupomNome: 'MOUSE52'
+    },
+    {
+      primeNinja: false,
+      desconto: '7%',
+      categoria: '',
+      cupomNome: 'TECLADO'
+    },
+    {
+      primeNinja: true,
+      desconto: '5%',
+      categoria: '',
+      cupomNome: 'FESTA1'
+    },
+    {
+      primeNinja: false,
+      desconto: '80%',
+      categoria: 'placa de vídeo',
+      cupomNome: 'GERENTETAMALUCO'
+    },
+    {
+      primeNinja: false,
+      desconto: '15%',
+      categoria: 'casa > cama/mesa/banho',
+      cupomNome: 'MAMAEFELIZ'
+    },
+    {
+      primeNinja: false,
+      desconto: '5%',
+      categoria: 'casa > cozinha',
+      cupomNome: 'COZINHA'
+    }
+  ]
+
   return (
-    <div className="App">
-      <Container maxWidth="sm">
-        <Cupom />
+    <div className="App" style={{backgroundColor:'#0060b1'}}>
+      <Container maxWidth="md">
+        {cupoms.map(cupom => {
+          return <Cupom primeNinja={cupom.primeNinja} desconto={cupom.desconto} categoria={cupom.categoria} cupomNome={cupom.cupomNome} />
+        })}
       </Container>      
     </div>
   )
--- src/components/Cupom/index.jsx
+++ src/components/Cupom/index.jsx
@@ -1,27 +1,30 @@
 import React from "react";
-import { Card, CardHeader, CardContent, Button, Typography} from "@mui/material";
+import { Card, Chip, CardContent, Button, Typography} from "@mui/material";
 import "./index.css";
-import ImgNinja from "../../assets/ninja.jpg";
+import ImgNinja from "../../assets/ninja.svg";
+import CopyAllOutlinedIcon from '@mui/icons-material/CopyAllOutlined';
+import Avatar from '@mui/material/Avatar';
 
-export default function Cupom() {
+export default function Cupom({primeNinja, desconto, categoria, cupomNome}) {
+
     return (
         <div className="cupom">
             
                 <Card className="card">
                     <CardContent>
-                        <img className="img-ninja-prime" src={ImgNinja}></img>
-                        <Typography variant='h3' sx={{ color: "white", fontWeight: "bold" }} component="div" align="center">5%</Typography>
-                        <Typography variant='h6' sx={{ marginTop: "25px", fontWeight: "bold", color: "white" }} component="div">Periférico {'>'} Mouse</Typography>
+                        {primeNinja ? <Chip className="chip-prime-ninja" avatar={<Avatar className="img-ninja" src={ImgNinja}/>} label="Prime Ninja" /> : ''}
+                        <Typography variant='h3' sx={{fontWeight: "bold" }} component="div" align="center">{desconto}</Typography>
+                        <Typography variant='h6' sx={{ marginTop: "25px", fontWeight: "bold" }} component="div">{categoria}</Typography>
                     </CardContent>
                 </Card>
                 <Card className="card card-right">
                     <CardContent>
                         <div>
-                            <Typography variant='h5' sx={{ color: "white" }} component="div" align="center">
+                            <Typography variant='h5' component="div" align="center">
                                 Use o Cupom
                             </Typography>
-                            <Button onClick={() => window.location.href = "https://www.kabum.com.br"} variant='contained' sx={{ fontSize: 18, color: "white" }} className="botao-codigo">
-                                MOUSE52
+                            <Button onClick={() => {navigator.clipboard.writeText(cupomNome)}} variant='contained' sx={{ fontSize: 18}} className="botao-codigo">
+                                <CopyAllOutlinedIcon/> {cupomNome}
                             </Button>
                         </div>
                     </CardContent>
